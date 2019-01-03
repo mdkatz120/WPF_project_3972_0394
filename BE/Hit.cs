671e4f299927f1053a4f1854f910df79603f2534
David Katz <mdkatz120@walla.com>
2019-01-03 14:00:35 +0200

    step2 grid,menu,BE start

---
+++ BE/Hit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BE
+{
+    public class Hit
+    {
+        private DateTime hour ;
+        public string name { get; set; }
+        public string address { get; set; }
+        public int hitNum { get; set; }
+
+        public DateTime Hour
+        {
+            get { return hour; }
+            set { hour = value; }
+        }  
+            
+    }
+}
