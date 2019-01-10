dff1352aab93a35b15e906af4ba7871f9fcf7267
Emanuel Lozitky <shtainrubin@gmail.com>
2019-01-10 13:07:34 +0200

    has been added bing map

--- WPF_project_3972_0394/MainWindow.xaml.cs
+++ WPF_project_3972_0394/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Maps.MapControl.WPF;
 
 
 namespace WPF_project_3972_0394
